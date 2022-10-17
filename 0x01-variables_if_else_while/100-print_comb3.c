1561c2fd8561f78686bc68374caf96032d4fdde6
EmmylsonTech <ehis.emmylson@gmail.com>
2022-10-17 08:22:20 -0700

    task 10 to 13

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,18 +8,20 @@ int main(void)
 {
 	int n, m;
 
-	for (n = 48; n < 57; n++)
+	for (n = 48; n <= 56; n++)
 	{
-		for (m = 48; m < 57; m++)
+		for (m = 49; m < 57; m++)
 		{
-			putchar(n);
-			putchar(m);
-			if (n == 56 && m == 56)
-				continue;
-		
-			putchar(',');
-			putchar(' ');
-			
+			if (m > n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 	putchar('\n');
