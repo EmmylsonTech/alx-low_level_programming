c32c1e647de6b73c7e4a172c279ed0e294e6f443
EmmylsonTech <ehis.emmylson@gmail.com>
2022-10-19 00:24:00 -0700

    task 0 to 5

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,11 +2,13 @@
 
 /**
  * main -  prints _putchar, followed by a new line
+ * is written by EmmylsonTech
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-_putchar("_");
+_putchar('_');
 _putchar('p');
 _putchar('u');
 _putchar('t');
@@ -14,5 +16,6 @@ _putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('r');
+_putchar('\n');
 return (0);
 }
